bfe5764856439adf90fe4465c2c3c811ac55415b
Dario de Paula MAciel <dariodepaulamaciel@hotmail.com>
2023-01-18 09:06:06 -0300

    criação de tela de entrada no app

---
+++ android/app/src/main/kotlin/com/example/beaultiful_login_and_slidable_drawer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.beaultiful_login_and_slidable_drawer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/entrada/entrada.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Entrada extends StatefulWidget {
+  const Entrada({super.key});
+
+  @override
+  State<Entrada> createState() => _EntradaState();
+}
+
+class _EntradaState extends State<Entrada> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
